f938f40f1d811975585367f362659fcfd8d07fd5
Mayuresh_22 <mayureshchoudhary22@gmail.com>
2023-08-18 20:20:39 +0530

    Made Few changes

--- Layout/LoadingLayout.java
+++ Layout/LoadingLayout.java
@@ -1,5 +1,13 @@
 package Layout;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class LoadingLayout {
+    JPanel LoadingPanel = new JPanel(new BorderLayout());
+    JLabel bg;
     
+    LoadingLayout(){
+        LoadingPanel.setIcon
+    }
 }
