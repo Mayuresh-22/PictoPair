0d464b7694aa7745429e8c6975ff20fa447ab74e
Mayuresh_22 <mayureshchoudhary22@gmail.com>
2023-08-18 21:50:13 +0530

    Made Few changes

--- Layout/LoadingLayout.java
+++ Layout/LoadingLayout.java
@@ -9,17 +9,18 @@ public class LoadingLayout {
     JLayeredPane layeredPane;
     
     public LoadingLayout(){
-        // backgroun panel
-        bg = new JLabel(new ImageIcon("assets/LoadingScreen-bg.png"));
-        bgPanel.add(bg, BorderLayout.CENTER);
-        // logo panel
-        logoPanel.setPreferredSize(new Dimension(300, 300));
-        logo = new JLabel(new ImageIcon("assets/logo.jpeg"));
-        logoPanel.add(logo, BorderLayout.CENTER);
+        // // backgroun panel
+        // bg = new JLabel(new ImageIcon("assets/LoadingScreen-bg.png"));
+        // bgPanel.add(bg, BorderLayout.CENTER);
+        // // logo panel
+        // logoPanel.setPreferredSize(new Dimension(300, 300));
+        // logo = new JLabel(new ImageIcon("assets/logo.jpeg"));
+        // logoPanel.add(logo, BorderLayout.CENTER);
 
         // create layered pane
         layeredPane = new JLayeredPane();
         layeredPane.setBounds(0, 0, 500, 500);
+        layeredPane.setBackground(Color.gray);
 
         LoadingPanel.add(layeredPane);
         LoadingPanel.setVisible(true);
