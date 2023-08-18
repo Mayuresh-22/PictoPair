4dabdc451b3d3c4799dbe7b57ace4fb803ce6c1c
Mayuresh_22 <mayureshchoudhary22@gmail.com>
2023-08-18 23:05:52 +0530

    Made Few changes

--- Layout/HomeLayout.java
+++ Layout/HomeLayout.java
@@ -25,7 +25,7 @@ public class HomeLayout {
         // Adding to layeredPane
         layeredPane.add(bgPanel);
 
-        // Adding to HomePanl
+        // Adding to HomePanel
         HomePanel.add(layeredPane);
         HomePanel.setOpaque(true);
     }
