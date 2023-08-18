126d4f5509f4de655eb63b2a1e9295300796c576
Mayuresh_22 <mayureshchoudhary22@gmail.com>
2023-08-18 20:41:52 +0530

    Made Few changes

--- Layout/LoadingLayout.java
+++ Layout/LoadingLayout.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class LoadingLayout {
-    JPanel LoadingPanel, LoadingLogo = new JPanel(new BorderLayout());
+    JPanel LoadingPanel, LoadingLogoPanel = new JPanel(new BorderLayout());
     JLayeredPane LoadingLayoutPane;
     JLabel bg, logo;
     
