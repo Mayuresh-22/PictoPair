53784350298e725b38288aa99d622e32d20bdc10
Mayuresh_22 <mayureshchoudhary22@gmail.com>
2023-08-18 22:00:36 +0530

    Made Few changes

--- PictoPair.java
+++ PictoPair.java
@@ -10,17 +10,6 @@ class App {
         app.setResizable(false);
         app.setLayout(null);
 
-        JLayeredPane layeredPane = new JLayeredPane();
-        layeredPane.setSize(500, 500);
-        layeredPane.setOpaque(true);
-        layeredPane.setBackground(Color.red);
-
-        JLabel bg = new JLabel();
-        bg.setBounds(100, 100, 100, 100);
-        bg.setBackground(Color.black);
-        bg.setOpaque(true);
-        layeredPane.add(bg);
-
         app.add(layeredPane);
         app.setVisible(true);
     }
