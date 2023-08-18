d0b0d7ff719fd3dcae8fc0e9cfefe9927016a18c
Mayuresh_22 <mayureshchoudhary22@gmail.com>
2023-08-18 22:02:18 +0530

    Made Few changes

--- Layout/LoadingLayout.java
+++ Layout/LoadingLayout.java
@@ -10,6 +10,7 @@ public class LoadingLayout {
     public LoadingLayout(){
         bg = new JLabel();
         bg.setBounds(100, 100, 100, 100);
+        bg.setOpaque(true);
         bg.setBackground(Color.red);
         LoadingPanel.add(bg);
     }
