8471baf930feb5ffb84377239c5968413484b615
Mayuresh_22 <mayureshchoudhary22@gmail.com>
2023-08-18 22:06:32 +0530

    Made Few changes

--- Layout/LoadingLayout.java
+++ Layout/LoadingLayout.java
@@ -10,12 +10,13 @@ public class LoadingLayout {
     public LoadingLayout(){
         JLayeredPane layeredPane = new JLayeredPane();
         layeredPane.setSize(JFrame.MAXIMIZED_HORIZ, JFrame.MAXIMIZED_VERT);
+
         bg = new JLabel(new ImageIcon("assets/LoadingScreen-bg.png"));
         bgPanel.setBounds(0, 0, 1920, 1080);
         bgPanel.add(bg);
 
 
-        LoadingPanel.add(bg);
+        LoadingPanel.add(layeredPane);
     }
 
     public JPanel getLoadingPanel() {
