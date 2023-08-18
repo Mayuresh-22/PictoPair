ec45ab1bba5442fcae15a38a67bfaa5eb6bbc66a
Mayuresh_22 <mayureshchoudhary22@gmail.com>
2023-08-18 20:29:27 +0530

    Made Few changes

--- PictoPair.java
+++ PictoPair.java
@@ -4,10 +4,12 @@ import Layout.*;
 
 class App {
     JFrame app = new JFrame("PictoPair - Memory Game");
+    LoadingLayout loadingPanel = new LoadingLayout();
 
     App(){
         app.setExtendedState(JFrame.MAXIMIZED_BOTH);
         app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        app.add()
     }
 }
 
