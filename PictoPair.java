de4966e20338343406cf73f9efd749f8912523a9
Mayuresh_22 <mayureshchoudhary22@gmail.com>
2023-08-18 20:27:52 +0530

    Made Few changes

--- PictoPair.java
+++ PictoPair.java
@@ -3,7 +3,12 @@ import javax.swing.*;
 import Layout.*;
 
 class App {
-    JFrame app = new JFrame("PictoPair - Memory Game")
+    JFrame app = new JFrame("PictoPair - Memory Game");
+
+    App(){
+        app.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+        app.setUndecorated(true);
+    }
 }
 
 
