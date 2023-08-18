3f0978222d7d379f42ed590fe34b16acd3de5ead
Mayuresh_22 <mayureshchoudhary22@gmail.com>
2023-08-18 20:55:56 +0530

    Made Few changes

--- Layout/LoadingLayout.java
+++ Layout/LoadingLayout.java
@@ -14,9 +14,9 @@ public class LoadingLayout {
         bg = new JLabel(new ImageIcon("assets/LoadingScreen-bg.png"));
         LoadingBgPanel.add(bg, BorderLayout.CENTER);
         // create a logo panel
-        LoadingLogoPanel.setSize(200, 200);
-        logo = new JLabel(new ImageIcon("assets/logo.jpeg"));
-        LoadingLogoPanel.add(logo, BorderLayout.CENTER);
+        // LoadingLogoPanel.setSize(200, 200);
+        // logo = new JLabel(new ImageIcon("assets/logo.jpeg"));
+        // LoadingLogoPanel.add(logo, BorderLayout.CENTER);
 
         LoadingLayoutPane.add(LoadingBgPanel, BorderLayout.CENTER);
         // LoadingLayoutPane.add(LoadingLogoPanel, BorderLayout.CENTER);
