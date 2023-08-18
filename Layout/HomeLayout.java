491b166e4411dc19fc206a5a0b7a92488760da77
Mayuresh_22 <mayureshchoudhary22@gmail.com>
2023-08-18 23:04:18 +0530

    Made Few changes

--- Layout/HomeLayout.java
+++ Layout/HomeLayout.java
@@ -5,6 +5,7 @@ import java.awt.*;
 
 public class HomeLayout {
     JPanel HomePanel = new JPanel(null), bgPanel = new JPanel(new BorderLayout());
+    JLabel bg
 
     public HomeLayout(){
         JLayeredPane layeredPane = new JLayeredPane();
@@ -14,6 +15,13 @@ public class HomeLayout {
         layeredPane.setSize(width, height);
         layeredPane.setBackground(Color.gray);
         layeredPane.setOpaque(true);
+
+        // bgPanel
+        bg = new JLabel(new ImageIcon("assets/LoadingScreen-bg.png"));
+        bgPanel.setBounds(0, 0, width, height);
+        bgPanel.setOpaque(true);
+        bgPanel.add(bg);
+
 
         HomePanel.add(layeredPane);
         HomePanel.setOpaque(true);
