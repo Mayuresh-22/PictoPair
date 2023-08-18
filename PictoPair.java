46adea9ae34f7496e3738a40044f26649615262f
Mayuresh_22 <mayureshchoudhary22@gmail.com>
2023-08-18 21:57:40 +0530

    Made Few changes

--- PictoPair.java
+++ PictoPair.java
@@ -9,6 +9,7 @@ class App {
         app.setExtendedState(JFrame.MAXIMIZED_BOTH);
         app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         app.setResizable(false);
+        app.setLayout(null);
 
         JLayeredPane layeredPane = new JLayeredPane();
         layeredPane.setSize(500, 500);
