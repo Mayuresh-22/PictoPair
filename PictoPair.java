d7323f223184db7e0fc602448b31518eab016b74
Mayuresh_22 <mayureshchoudhary22@gmail.com>
2023-08-18 22:25:58 +0530

    Made Few changes

--- PictoPair.java
+++ PictoPair.java
@@ -5,7 +5,7 @@ import Layout.*;
 class App {
     JFrame app = new JFrame("PictoPair - Memory Game");
 
-    App(){
+    App() throws InterruptedException{
         app.setExtendedState(JFrame.MAXIMIZED_BOTH);
         app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         app.setResizable(false);
