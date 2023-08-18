20e56b96418cc1285e3d3ea98710639fe041d367
Mayuresh_22 <mayureshchoudhary22@gmail.com>
2023-08-18 22:23:22 +0530

    Made Few changes

--- Layout/LoadingLayout.java
+++ Layout/LoadingLayout.java
@@ -24,7 +24,6 @@ public class LoadingLayout {
 
         // logoPanel
         logo = new JLabel(new ImageIcon("assets/logo.jpeg"));
-        // center logo
         logoPanel.setBounds((width/2)-200, (height/2)-200, 400, 400);
         logoPanel.add(logo);
 
