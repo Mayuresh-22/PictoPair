d5dd07b49fc33be7f3cca41069cd30beb80797b6
Mayuresh_22 <mayureshchoudhary22@gmail.com>
2023-08-18 22:53:39 +0530

    Made Few changes

--- Layout/HomeLayout.java
+++ Layout/HomeLayout.java
@@ -5,5 +5,16 @@ import java.awt.*;
 
 public class HomeLayout {
     JPanel HomePanel = new JPanel(null);
-    
+
+    HomeLayout(){
+        JLayeredPane layeredPane = new JLayeredPane();
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = (int) screenSize.getWidth();
+        int height = (int) screenSize.getHeight();
+        layeredPane.setSize(width, height);
+        layeredPane.setBackground(Color.gray);
+        layeredPane.setOpaque(true);
+
+        HomePanel.add(layeredPane);
+    }
 }
