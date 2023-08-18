f52c79f8e51b79e97f9d75c42b3bb4134c335599
Mayuresh_22 <mayureshchoudhary22@gmail.com>
2023-08-18 21:05:00 +0530

    Made Few changes

--- PictoPair.java
+++ PictoPair.java
@@ -9,6 +9,7 @@ class App {
     App(){
         app.setExtendedState(JFrame.MAXIMIZED_BOTH);
         app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        app.setResizable(false);
         app.setLayout(new BorderLayout());
         app.add(loadingPanel.getLoadingPanel(), BorderLayout.CENTER);
         app.setVisible(true);
