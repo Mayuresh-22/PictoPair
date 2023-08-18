36b7a61295002fd54f609ef2a1121d698d10056d
Mayuresh_22 <mayureshchoudhary22@gmail.com>
2023-08-18 20:25:05 +0530

    Made Few changes

--- PictoPair.java
+++ PictoPair.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 import Layout.*;
 
 class PictoPair {
-    JFrame app = new JFrame("PictoPair - Memory Game")
+    JFrame app = new JFrame("PictoPair - Memory Game");
     public static void main(String[] args) {
         
     }
