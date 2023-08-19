9147e325965dc8caf7029342713a768f9f0d2a5a
Devang Gentyal <darshangentyal02@gmail.com>
2023-08-19 15:23:04 +0530

    Made Few changes

--- layout/GameLayout.java
+++ layout/GameLayout.java
@@ -51,6 +51,7 @@ public class GameLayout implements ScreenStructure {
 
     public void createCardsPanel(){
         cardsPanel.setBounds((int)(ScreenStructure.WIDTH*0.03),(int)(ScreenStructure.HEIGHT*0.03), (int)(ScreenStructure.WIDTH*0.7), (int)(ScreenStructure.HEIGHT*0.9));
+        cardsPanel.setBackground(Color.WHITE);
         cardsPanel.setOpaque(true);
         cardsPanel.setBorder(new TextBubbleBorder(new Color(200, 200, 200), 10, 70, 0));
     }
