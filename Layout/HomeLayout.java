5abd55ae98c733bfa7dc32542f2a77137dca5a00
Mayuresh_22 <mayureshchoudhary22@gmail.com>
2023-08-19 11:03:16 +0530

    Made Few changes

--- Layout/HomeLayout.java
+++ Layout/HomeLayout.java
@@ -23,7 +23,6 @@ public class HomeLayout {
         bgPanel.add(bg);
 
         // menu panel
-        menuPanel.setSize(500, 500);
         menuPanel.setBounds((width/2)-500, (height/2)-500, 400, 400);
         menuPanel.setBackground(Color.gray);
         menuPanel.setOpaque(true);
