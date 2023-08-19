7cf13cf2d4691136d978c175c16d4a3c39e6764a
Devang Gentyal <darshangentyal02@gmail.com>
2023-08-19 14:28:06 +0530

    Made Few changes

--- layout/GameLayout.java
+++ layout/GameLayout.java
@@ -6,7 +6,7 @@ import java.awt.*;
 import interfaces.*;
 
 public class GameLayout implements ScreenStructure {
-    JPanel GamePanel = new JPanel(null), bgPanel = new JPanel(new BorderLayout()), menuPanel = new JPanel(new FlowLayout(0));
+    JPanel GamePanel = new JPanel(null), bgPanel = new JPanel(new BorderLayout()), cardsPanel = new JPanel(new FlowLayout(0));
     JLabel bg;
     JLayeredPane layeredPane = new JLayeredPane();
 
