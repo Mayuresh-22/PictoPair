658fc534a76f400c473ac35d048f07a509a4dc93
Devang Gentyal <darshangentyal02@gmail.com>
2023-08-19 15:22:20 +0530

    Made Few changes

--- layout/GameLayout.java
+++ layout/GameLayout.java
@@ -1,44 +1,9 @@
 package layout;
 
-// import external.*;
+import external.*;
 import javax.swing.*;
 import java.awt.*;
 import interfaces.*;
-import javax.swing.border.AbstractBorder;
-import javax.swing.border.Border;
-
-// RoundBorders
-class RoundedBorder extends AbstractBorder {
-    private int radius;
-
-    public RoundedBorder(int radius) {
-        this.radius = radius;
-    }
-
-    @Override
-    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
-        Graphics2D g2 = (Graphics2D) g.create();
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-        Color borderColor = Color.GRAY; // Customize the border color here
-
-        g2.setColor(borderColor);
-        g2.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
-
-        g2.dispose();
-    }
-
-    @Override
-    public Insets getBorderInsets(Component c) {
-        return new Insets(radius, radius, radius, radius);
-    }
-
-    @Override
-    public Insets getBorderInsets(Component c, Insets insets) {
-        insets.left = insets.top = insets.right = insets.bottom = radius;
-        return insets;
-    }
-}
 
 public class GameLayout implements ScreenStructure {
     JPanel GamePanel = new JPanel(null), bgPanel = new JPanel(new BorderLayout()), cardsPanel = new JPanel(), scorePanel = new JPanel();
@@ -87,8 +52,6 @@ public class GameLayout implements ScreenStructure {
     public void createCardsPanel(){
         cardsPanel.setBounds((int)(ScreenStructure.WIDTH*0.03),(int)(ScreenStructure.HEIGHT*0.03), (int)(ScreenStructure.WIDTH*0.7), (int)(ScreenStructure.HEIGHT*0.9));
         cardsPanel.setOpaque(true);
-        cardsPanel.setBackground(new Color(0, 0, 0, 0));
-        Border roundedBorder = new RoundedBorder(20);
-        cardsPanel.setBorder(roundedBorder);
+        cardsPanel.setBorder(new TextBubbleBorder(new Color(200, 200, 200), 10, 70, 0));
     }
 }
