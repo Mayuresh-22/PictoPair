da801af60edf015172d8efd78a494c19abd84163
Devang Gentyal <darshangentyal02@gmail.com>
2023-08-19 15:13:26 +0530

    Made Few changes

--- layout/GameLayout.java
+++ layout/GameLayout.java
@@ -5,7 +5,37 @@ import javax.swing.*;
 import java.awt.*;
 import interfaces.*;
 import javax.swing.border.AbstractBorder;
+public class RoundedBorder extends AbstractBorder {
+    private int radius;
 
+    public RoundedBorder(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        Color borderColor = Color.GRAY; // Customize the border color here
+
+        g2.setColor(borderColor);
+        g2.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
+
+        g2.dispose();
+    }
+
+    @Override
+    public Insets getBorderInsets(Component c) {
+        return new Insets(radius, radius, radius, radius);
+    }
+
+    @Override
+    public Insets getBorderInsets(Component c, Insets insets) {
+        insets.left = insets.top = insets.right = insets.bottom = radius;
+        return insets;
+    }
+}
 
 public class GameLayout implements ScreenStructure {
     JPanel GamePanel = new JPanel(null), bgPanel = new JPanel(new BorderLayout()), cardsPanel = new JPanel(), scorePanel = new JPanel();
