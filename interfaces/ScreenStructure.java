05e4c4e7e5bd45e2184ca2c41d895819ac8d8156
Mayuresh_22 <mayureshchoudhary22@gmail.com>
2023-08-19 11:17:04 +0530

    Made Few changes

--- interfaces/ScreenStructure.java
+++ interfaces/ScreenStructure.java
@@ -1,6 +1,12 @@
 package interfaces;
 
+import java.awt.Dimension;
+
 public interface ScreenStructure {
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    int width = (int) screenSize.getWidth();
+    int height = (int) screenSize.getHeight();
     public void createLayeredPane();
     public void createBgPanel(String imagePath);
+    public static 
 }
