0b54ab8f051db1be954af939edfeddc1bdbe9af7
Devang Gentyal <darshangentyal02@gmail.com>
2023-08-20 20:33:17 +0530

    Made Few changes

--- layout/GameLayout.java
+++ layout/GameLayout.java
@@ -135,6 +135,8 @@ public class GameLayout implements ScreenStructure {
         // Creating Score Labels
         JLabel matches = new JLabel("Matches : 0");
         JLabel turns = new JLabel("Turns : 0");
+        matches.setFont(labelFont);
+        matches.setFont(labelFont);
 
         // Styling Labels
         matches.setSize(200, 100);
