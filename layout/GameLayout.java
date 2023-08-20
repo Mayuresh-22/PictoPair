712f48466ee3bed940fdf8a31dd336b2d58e0436
Devang Gentyal <darshangentyal02@gmail.com>
2023-08-20 21:04:35 +0530

    Made Few changes

--- layout/GameLayout.java
+++ layout/GameLayout.java
@@ -156,33 +156,29 @@ public class GameLayout implements ScreenStructure {
         cardsPanel.setLayout(new GridLayout(4,6,20,20));
     }
 
-    static class GradientLabel extends JLabel {
+    static public class GradientLabel extends JLabel {
         private Color startColor;
         private Color endColor;
-    
-        public class GradientLabel extends JLabel {
-            private Color startColor;
-            private Color endColor;
+        
+        public GradientLabel(String text, Color startColor, Color endColor) {
+            super(text);
+            this.startColor = startColor;
+            this.endColor = endColor;
+            setOpaque(false); // Make the label transparent
+        }
+        
+        @Override
+        protected void paintComponent(Graphics g) {
+            Graphics2D g2d = (Graphics2D) g.create();
+            g2d.setPaint(new GradientPaint(
+                    new Point2D.Float(0, 0), startColor,
+                    new Point2D.Float(0, getHeight()), endColor
+            ));
+            g2d.fillRect(0, 0, getWidth(), getHeight());
+            g2d.dispose(); // Dispose of the graphics context
             
-            public GradientLabel(String text, Color startColor, Color endColor) {
-                super(text);
-                this.startColor = startColor;
-                this.endColor = endColor;
-                setOpaque(false); // Make the label transparent
-            }
-            
-            @Override
-            protected void paintComponent(Graphics g) {
-                Graphics2D g2d = (Graphics2D) g.create();
-                g2d.setPaint(new GradientPaint(
-                        new Point2D.Float(0, 0), startColor,
-                        new Point2D.Float(0, getHeight()), endColor
-                ));
-                g2d.fillRect(0, 0, getWidth(), getHeight());
-                g2d.dispose(); // Dispose of the graphics context
-                
-                super.paintComponent(g); // Paint the label's text on top
-            }
+            super.paintComponent(g); // Paint the label's text on top
         }
     }
+    
 }
