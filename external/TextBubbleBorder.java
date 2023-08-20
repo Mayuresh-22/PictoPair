a769607abddd5b3068e46bf5cad2c6b2bb917b05
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-08-20 12:32:29 +0530

    Made Few changes

--- external/TextBubbleBorder.java
+++ external/TextBubbleBorder.java
@@ -14,13 +14,16 @@ public class TextBubbleBorder extends AbstractBorder {
     private BasicStroke stroke = null;
     private int strokePad;
     private int pointerPad = 4;
+    private boolean left = true;
     RenderingHints hints;
 
-    public TextBubbleBorder(Color color) {
-        new TextBubbleBorder(color, 4, 8, 7);
+    TextBubbleBorder(
+            Color color) {
+        this(color, 4, 8, 7);
     }
 
-    public TextBubbleBorder(Color color, int thickness, int radii, int pointerSize) {
+    TextBubbleBorder(
+            Color color, int thickness, int radii, int pointerSize) {
         this.thickness = thickness;
         this.radii = radii;
         this.pointerSize = pointerSize;
@@ -36,6 +39,12 @@ public class TextBubbleBorder extends AbstractBorder {
         int pad = radii + strokePad;
         int bottomPad = pad + pointerSize + strokePad;
         insets = new Insets(pad, pad, bottomPad, pad);
+    }
+
+    TextBubbleBorder(
+            Color color, int thickness, int radii, int pointerSize, boolean left) {
+        this(color, thickness, radii, pointerSize);
+        this.left = left;
     }
 
     @Override
@@ -69,29 +78,52 @@ public class TextBubbleBorder extends AbstractBorder {
 
         Polygon pointer = new Polygon();
 
-        // left point
-        pointer.addPoint(
-                strokePad + radii + pointerPad,
-                bottomLineY);
-        // right point
-        pointer.addPoint(
-                strokePad + radii + pointerPad + pointerSize,
-                bottomLineY);
-        // bottom point
-        pointer.addPoint(
-                strokePad + radii + pointerPad + (pointerSize / 2),
-                height - strokePad);
+        if (left) {
+            // left point
+            pointer.addPoint(
+                    strokePad + radii + pointerPad,
+                    bottomLineY);
+            // right point
+            pointer.addPoint(
+                    strokePad + radii + pointerPad + pointerSize,
+                    bottomLineY);
+            // bottom point
+            pointer.addPoint(
+                    strokePad + radii + pointerPad + (pointerSize / 2),
+                    height - strokePad);
+        } else {
+            // left point
+            pointer.addPoint(
+                    width - (strokePad + radii + pointerPad),
+                    bottomLineY);
+            // right point
+            pointer.addPoint(
+                    width - (strokePad + radii + pointerPad + pointerSize),
+                    bottomLineY);
+            // bottom point
+            pointer.addPoint(
+                    width - (strokePad + radii + pointerPad + (pointerSize / 2)),
+                    height - strokePad);
+        }
 
         Area area = new Area(bubble);
         area.add(new Area(pointer));
 
         g2.setRenderingHints(hints);
 
-        Area spareSpace = new Area(new Rectangle(0, 0, width, height));
-        spareSpace.subtract(area);
-        g2.setClip(spareSpace);
-        g2.clearRect(0, 0, width, height);
-        g2.setClip(null);
+        // Paint the BG color of the parent, everywhere outside the clip
+        // of the text bubble.
+        Component parent  = c.getParent();
+        if (parent!=null) {
+            Color bg = parent.getBackground();
+            Rectangle rect = new Rectangle(0,0,width, height);
+            Area borderRegion = new Area(rect);
+            borderRegion.subtract(area);
+            g2.setClip(borderRegion);
+            g2.setColor(bg);
+            g2.fillRect(0, 0, width, height);
+            g2.setClip(null);
+        }
 
         g2.setColor(color);
         g2.setStroke(stroke);
