1fd9e686466c893dda5f5e085787bd895261b8aa
Devang Gentyal <darshangentyal02@gmail.com>
2023-08-20 17:00:20 +0530

    Made Few changes

--- layout/GameLayout.java
+++ layout/GameLayout.java
@@ -123,11 +123,11 @@ public class GameLayout implements ScreenStructure {
     public void createScorePanel() {
 
         // Creating Score Panel
-        cardsPanel.setBounds((int) (ScreenStructure.WIDTH * 0.03), (int) (ScreenStructure.HEIGHT * 0.03),
+        scorePanel.setBounds((int) (ScreenStructure.WIDTH * 0.03), (int) (ScreenStructure.HEIGHT * 0.03),
                 (int) (ScreenStructure.WIDTH * 0.7), (int) (ScreenStructure.HEIGHT * 0.9));
-        cardsPanel.setBackground(Color.WHITE);
-        cardsPanel.setOpaque(true);
-        cardsPanel.setBorder(new TextBubbleBorder(new Color(200, 200, 200), 10, 70, 0));
-        cardsPanel.setLayout(new GridLayout(4, 6, 20, 20));
+        scorePanel.setBackground(Color.WHITE);
+        scorePanel.setOpaque(true);
+        scorePanel.setBorder(new TextBubbleBorder(new Color(200, 200, 200), 10, 70, 0));
+        scorePanel.setLayout(new GridLayout(4, 6, 20, 20));
     }
 }
