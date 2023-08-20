6a68f34c6526b0a10a2777daceda8202b8f226a4
Devang Gentyal <darshangentyal02@gmail.com>
2023-08-20 16:44:13 +0530

    Made Few changes

--- external/Cards.java
+++ external/Cards.java
@@ -17,7 +17,7 @@ public class Cards implements ActionListener {
 
 
 
-    Cards(String status, ImageIcon mainImg, ImageIcon defaultImg, String path) {
+    public Cards(String status, ImageIcon mainImg, ImageIcon defaultImg, String path) {
 
         button = new JButton(defaultImg);
         button.setBorderPainted(false);
