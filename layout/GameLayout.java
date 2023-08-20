473969e4c81b29c1433d658ca32b288052c6f194
Devang Gentyal <darshangentyal02@gmail.com>
2023-08-20 14:15:18 +0530

    Made Few changes

--- layout/GameLayout.java
+++ layout/GameLayout.java
@@ -150,7 +150,7 @@ public class GameLayout implements ScreenStructure {
 
         // Initializing Cards
         for (int i = 0; i < 24; i++) {
-            card[i] = new Cards("hidden", ("assets/images/card-back.png"), defaultImgIcon);
+            card[i] = new Cards("hidden", ("assets/images/card-back.png"), defaultImg);
             cardsPanel.add(card[i].button);
         }
     }
