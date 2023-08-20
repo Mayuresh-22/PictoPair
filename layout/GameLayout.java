a4f4ffa03d666f937b8523e027392429e210efd0
Devang Gentyal <darshangentyal02@gmail.com>
2023-08-20 16:23:11 +0530

    Made Few changes

--- layout/GameLayout.java
+++ layout/GameLayout.java
@@ -17,11 +17,11 @@ class Cards implements ActionListener {
 
     int id;
     JButton button;
-    String status,path;
-    ImageIcon defaultImg,mainImg;
+    String status, path;
+    ImageIcon defaultImg, mainImg;
     static MusicPlayerThread soundEffect = new MusicPlayerThread();
 
-    Cards(String status, ImageIcon mainImg, ImageIcon defaultImg,String path) {
+    Cards(String status, ImageIcon mainImg, ImageIcon defaultImg, String path) {
 
         button = new JButton(defaultImg);
         button.setBorderPainted(false);
@@ -55,12 +55,18 @@ class Cards implements ActionListener {
 
                 Timer timer = new Timer(500, new ActionListener() {
                     public void actionPerformed(ActionEvent e) {
-                        // Storing selected cards into temporary card objects and clearing the selected Vector
+                        // Storing selected cards into temporary card objects and clearing the selected
+                        // Vector
                         Cards c1 = selected.get(0);
                         Cards c2 = selected.get(1);
                         selected.clear();
-                    
+
                         // Playing Correct Sound Effect
+                        try {
+                            thread.join(); // Wait for the thread to finish
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
                         soundEffect.filePath = "assets/sounds/correct.wav";
                         soundEffect.loop = false;
                         soundEffect.start();
@@ -76,11 +82,12 @@ class Cards implements ActionListener {
             } else {
                 Timer timer = new Timer(1000, new ActionListener() {
                     public void actionPerformed(ActionEvent e) {
-                        // Storing selected cards into temporary card objects and clearing the selected Vector
+                        // Storing selected cards into temporary card objects and clearing the selected
+                        // Vector
                         Cards c3 = selected.get(0);
                         Cards c4 = selected.get(1);
                         selected.clear();
-                    
+
                         // Playing Wrong Sound Effect
                         soundEffect.filePath = "assets/sounds/wrong.wav";
                         soundEffect.loop = false;
@@ -103,11 +110,12 @@ class Cards implements ActionListener {
 // Main Class
 public class GameLayout implements ScreenStructure {
 
-    JPanel GamePanel = new JPanel(null), bgPanel = new JPanel(new BorderLayout()), cardsPanel = new JPanel(), scorePanel = new JPanel();
+    JPanel GamePanel = new JPanel(null), bgPanel = new JPanel(new BorderLayout()), cardsPanel = new JPanel(),
+            scorePanel = new JPanel();
     JLabel bg;
     JLayeredPane layeredPane = new JLayeredPane();
 
-    public GameLayout(){
+    public GameLayout() {
         createLayeredPane();
 
         createBgPanel("assets/images/GameLayout-bg.png");
@@ -129,7 +137,7 @@ public class GameLayout implements ScreenStructure {
     }
 
     @Override
-    public void createLayeredPane(){
+    public void createLayeredPane() {
         // layered pane
         layeredPane.setSize(ScreenStructure.WIDTH, ScreenStructure.HEIGHT);
         layeredPane.setBackground(Color.gray);
@@ -137,46 +145,53 @@ public class GameLayout implements ScreenStructure {
     }
 
     @Override
-    public void createBgPanel(String imagePath){
+    public void createBgPanel(String imagePath) {
         // bgPanel
-        bg = new JLabel(new ImageIcon(new ImageIcon(imagePath).getImage().getScaledInstance(ScreenStructure.WIDTH, ScreenStructure.HEIGHT, Image.SCALE_SMOOTH)));
+        bg = new JLabel(new ImageIcon(new ImageIcon(imagePath).getImage().getScaledInstance(ScreenStructure.WIDTH,
+                ScreenStructure.HEIGHT, Image.SCALE_SMOOTH)));
 
         bgPanel.setBounds(0, 0, ScreenStructure.WIDTH, ScreenStructure.HEIGHT);
         bgPanel.setOpaque(true);
         bgPanel.add(bg);
     }
 
-    public void createCardsPanel(){
+    public void createCardsPanel() {
         // Creating Cards Panel
-        cardsPanel.setBounds((int)(ScreenStructure.WIDTH*0.03),(int)(ScreenStructure.HEIGHT*0.03), (int)(ScreenStructure.WIDTH*0.7), (int)(ScreenStructure.HEIGHT*0.9));
+        cardsPanel.setBounds((int) (ScreenStructure.WIDTH * 0.03), (int) (ScreenStructure.HEIGHT * 0.03),
+                (int) (ScreenStructure.WIDTH * 0.7), (int) (ScreenStructure.HEIGHT * 0.9));
         cardsPanel.setBackground(Color.WHITE);
         cardsPanel.setOpaque(true);
         cardsPanel.setBorder(new TextBubbleBorder(new Color(200, 200, 200), 10, 70, 0));
-        cardsPanel.setLayout(new GridLayout(4,6,20,20));
+        cardsPanel.setLayout(new GridLayout(4, 6, 20, 20));
 
         // Creating Cards
-         Cards[] card = new Cards[24];
+        Cards[] card = new Cards[24];
 
         // Creating defaultImgIcon Variable
-        ImageIcon defaultImg = new ImageIcon("E:\\DarshanStudies\\Programing\\Java\\PictoPair-Memory-Game\\assets\\images\\card-back.png");
-        Image scaledImg = defaultImg.getImage().getScaledInstance((int)(ScreenStructure.WIDTH*0.1),(int)(ScreenStructure.HEIGHT*0.25), Image.SCALE_SMOOTH);
+        ImageIcon defaultImg = new ImageIcon(
+                "E:\\DarshanStudies\\Programing\\Java\\PictoPair-Memory-Game\\assets\\images\\card-back.png");
+        Image scaledImg = defaultImg.getImage().getScaledInstance((int) (ScreenStructure.WIDTH * 0.1),
+                (int) (ScreenStructure.HEIGHT * 0.25), Image.SCALE_SMOOTH);
         ImageIcon defaultImgIcon = new ImageIcon(scaledImg);
 
         int j = 1;
         // Initializing Cards
         for (int i = 0; i < 24; i++) {
-            if(j==13){
-                j=1;
+            if (j == 13) {
+                j = 1;
             }
-        // Creating mainImgIcon Variable
-         ImageIcon mainImg = new ImageIcon("E:\\DarshanStudies\\Programing\\Java\\PictoPair-Memory-Game\\assets\\images\\card"+j+".jpg");
-         Image mainscaledImg = mainImg.getImage().getScaledInstance((int)(ScreenStructure.WIDTH*0.1),(int)(ScreenStructure.HEIGHT*0.25), Image.SCALE_SMOOTH);
-         ImageIcon mainImgIcon = new ImageIcon(mainscaledImg);
+            // Creating mainImgIcon Variable
+            ImageIcon mainImg = new ImageIcon(
+                    "E:\\DarshanStudies\\Programing\\Java\\PictoPair-Memory-Game\\assets\\images\\card" + j + ".jpg");
+            Image mainscaledImg = mainImg.getImage().getScaledInstance((int) (ScreenStructure.WIDTH * 0.1),
+                    (int) (ScreenStructure.HEIGHT * 0.25), Image.SCALE_SMOOTH);
+            ImageIcon mainImgIcon = new ImageIcon(mainscaledImg);
 
-         card[i] = new Cards("hidden", mainImgIcon, defaultImgIcon,"E:\\DarshanStudies\\Programing\\Java\\PictoPair-Memory-Game\\assets\\images\\card"+j+".jpg");
-         cardsPanel.add(card[i].button);
+            card[i] = new Cards("hidden", mainImgIcon, defaultImgIcon,
+                    "E:\\DarshanStudies\\Programing\\Java\\PictoPair-Memory-Game\\assets\\images\\card" + j + ".jpg");
+            cardsPanel.add(card[i].button);
 
-         j++;
+            j++;
         }
     }
 }
