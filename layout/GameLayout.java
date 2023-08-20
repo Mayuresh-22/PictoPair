5b83f45b88dc67ca66abc9a2056e25fbcbc5dd73
Devang Gentyal <darshangentyal02@gmail.com>
2023-08-20 16:32:47 +0530

    Made Few changes

--- layout/GameLayout.java
+++ layout/GameLayout.java
@@ -90,7 +90,7 @@ class Cards implements ActionListener {
                         c3.status = "hidden";
                         c4.status = "hidden";
 
-                        soundEffect2.stop();
+                        soundEffect2.stopThread();
                     }
                 });
 
