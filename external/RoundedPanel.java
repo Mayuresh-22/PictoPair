f8687110dfb038bc4876c08d2d75d93cacf85f5d
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-08-20 12:12:34 +0530

    Made Few changes

--- external/RoundedPanel.java
+++ external/RoundedPanel.java
@@ -47,6 +47,6 @@ public class RoundedPanel extends JPanel
         }
         // graphics.fillRoundRect(0, 0, width-1, height-1, arcs.width, arcs.height); //paint background
         graphics.setColor(getForeground());
-        graphics.drawRoundRect(0, 0, width-1, height-1, arcs.width, arcs.height); //paint border
+        // graphics.drawRoundRect(0, 0, width-1, height-1, arcs.width, arcs.height); //paint border
     }
 }
