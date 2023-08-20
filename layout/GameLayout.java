0fbc893b0e5b3c608f078e4115d7917b69dd64ba
Devang Gentyal <darshangentyal02@gmail.com>
2023-08-20 21:35:56 +0530

    Made Few changes

--- layout/GameLayout.java
+++ layout/GameLayout.java
@@ -19,7 +19,7 @@ public class GameLayout implements ScreenStructure {
     JLabel bg;
     JLayeredPane layeredPane = new JLayeredPane();
 
-    public static int matches = 0,turns = 35;
+    public static int matches = 0,turns = 30;
 
     // Creating Score Labels
     public static JLabel matchesLabel = new JLabel("Matches : "+matches);
