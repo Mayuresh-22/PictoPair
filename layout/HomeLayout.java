6f15f4ee13468a7e97b0c69da5bbd43934873852
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-08-21 22:41:37 +0530

    Made Few changes

--- layout/HomeLayout.java
+++ layout/HomeLayout.java
@@ -112,7 +112,7 @@ public class HomeLayout implements ScreenStructure{
         dialog.setLocationRelativeTo(null);
         dialog.setLayout(new BorderLayout());
         dialog.setUndecorated(true);
-        dialog.getRootPane().setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4, new Color(131,0,255,255)));
+        dialog.getRootPane().setBorder(new TextBubbleBorder(new Color(131,0,255,255), 4, 70, 0));
         
         JLabel message = new JLabel("Are you sure you want to quit?");
         message.setFont(new Font("Arial", Font.BOLD, 30));
