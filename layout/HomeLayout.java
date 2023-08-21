9a13d99e02f5ba3c79d7350a0135748fb9fcf57f
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-08-21 22:00:34 +0530

    Made Few changes

--- layout/HomeLayout.java
+++ layout/HomeLayout.java
@@ -58,14 +58,7 @@ public class HomeLayout implements ScreenStructure{
         // with internal padding 10px and external padding 70px, bigger buttons
 
         // play button
-        play = new JButton("Play");
-        play.setBounds(0, 0, 500, 100);
-        play.setBackground(new Color(77,0,206,255));
-        play.setOpaque(true);
-        play.setBorderPainted(false);
-        play.setFocusPainted(false);
-        play.setForeground(Color.WHITE);
-        play.setFont(new Font("Arial", Font.BOLD, 40));
+        play = createButton(play, null, HEIGHT, HEIGHT, WIDTH, HEIGHT, HEIGHT)
         
         // settings button
         settings = new JButton("Settings");
