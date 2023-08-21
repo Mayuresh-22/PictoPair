d929a480466792251869605acf9b3b35bc837473
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-08-21 22:07:46 +0530

    Made Few changes

--- layout/HomeLayout.java
+++ layout/HomeLayout.java
@@ -12,7 +12,7 @@ public class HomeLayout implements ScreenStructure{
     JPanel HomePanel = new JPanel(null), bgPanel = new JPanel(new BorderLayout()), menuPanel = new JPanel(new FlowLayout(FlowLayout.CENTER,50, 40));
     JLabel bg;
     JLayeredPane layeredPane = new JLayeredPane();
-    JButton play, settings, quite;
+    JButton play, settings, quite, yes, no;
 
     public HomeLayout(){
         createLayeredPane();
