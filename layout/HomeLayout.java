0356b1d48a5943454e64fd1f5e158cee68acc90d
Devang Gentyal <darshangentyal02@gmail.com>
2023-08-27 13:17:40 +0530

    Made Few changes

--- layout/HomeLayout.java
+++ layout/HomeLayout.java
@@ -133,7 +133,7 @@ public class HomeLayout implements ScreenStructure{
 
         // Setting Options Panel
         JPanel optionsPanel = new JPanel();
-        optionsPanel.setLayout(new GridLayout(2,1));
+        optionsPanel.setLayout(new GridLayout(2,1,10,0));
 
         JButton yes = new JButton();
         yes = createButton(yes, "YES", 0, 0, 200, 100, 30);
