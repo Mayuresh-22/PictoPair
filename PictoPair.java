be0a6c68676edcb06bb3d017c697885e3905d56c
Devang Gentyal <darshangentyal02@gmail.com>
2023-08-27 12:51:02 +0530

    Made Few changes

--- PictoPair.java
+++ PictoPair.java
@@ -1,5 +1,58 @@
-// Main File to import all the classes and control flow
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 import layout.*;
+import external.*;
+
+// Devang App Changed - 04
+
+class App {
+    JFrame app = new JFrame("PictoPair - Memory Game");
+    LoadingLayout loadingLayout = new LoadingLayout();
+    HomeLayout homeLayout = new HomeLayout();
+    GameLayout gameLayout = new GameLayout();
+    MusicPlayerThread soundEffect = new MusicPlayerThread();
+    MusicPlayerThread musicPlayer = new MusicPlayerThread();
+
+    App(){
+        appConfig();
+        app.add(loadingLayout.getLoadingPanel(), BorderLayout.CENTER);
+        soundEffect.filePath = "assets/sounds/intro.wav";
+        soundEffect.loop = false;
+        soundEffect.start();
+
+        // Remove LoadingPanel after 5 seconds
+        Timer timer = new Timer(4900, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                app.remove(loadingLayout.getLoadingPanel());
+                app.add(homeLayout.getHomePanel(), BorderLayout.CENTER);
+                app.revalidate();
+                app.repaint();
+                musicPlayer.filePath = "assets/sounds/bgm.wav";
+                musicPlayer.loop = true;
+                musicPlayer.start();
+            }
+        });
+        timer.setRepeats(false);
+        timer.start();
+        
+        app.setVisible(true);
+    }
+
+    // app configuration
+    public void appConfig(){
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        app.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        app.setMinimumSize(screenSize);
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        app.setResizable(false);
+        app.setLayout(new BorderLayout(100, 100));
+        app.setIconImage(new ImageIcon("assets/images/card-back.png").getImage());
+    }
+
+}
+
 
 public class PictoPair {
     public static App mainApp;
