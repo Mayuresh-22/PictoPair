c649d7b1ec6efaa2b9479c898a395a65b1320526
Mayuresh <mayureshchoudhary22@gmail.com>
2023-08-28 13:43:00 +0530

    Made Few changes

--- PictoPair.java
+++ PictoPair.java
@@ -8,7 +8,6 @@ class App {
     JFrame app = new JFrame("PictoPair - Memory Game");
     LoadingLayout loadingLayout = new LoadingLayout();
     HomeLayout homeLayout = new HomeLayout(app);
-    GameLayout gameLayout = new GameLayout();
     MusicPlayerThread soundEffect = new MusicPlayerThread();
     MusicPlayerThread musicPlayer = new MusicPlayerThread();
     App(){
