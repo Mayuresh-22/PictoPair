4245e9a0ec0a892402fb797e0f8fa95845a10270
Devang Gentyal <darshangentyal02@gmail.com>
2023-08-30 13:54:30 +0530

    Made Few changes

--- layout/EndingLayout.java
+++ layout/EndingLayout.java
@@ -66,6 +66,7 @@ public class EndingLayout implements ScreenStructure{
         scoreLabel.setForeground(Color.WHITE);
 
         JPanel buttonsPanel = new JPanel(new FlowLayout(FlowLayout.CENTER,40, 40));
+        buttonsPanel.setB
 
         play = createButton(play, "PLAY", 0, 0, 200, 50, 20);
         play.addActionListener(new ActionListener(){
