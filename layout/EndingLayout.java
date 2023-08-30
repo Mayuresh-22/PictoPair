1942c155ce99fde54bc5ae7c9eacf2f635372d8f
Devang Gentyal <darshangentyal02@gmail.com>
2023-08-30 13:59:19 +0530

    Made Few changes

--- layout/EndingLayout.java
+++ layout/EndingLayout.java
@@ -60,7 +60,7 @@ public class EndingLayout implements ScreenStructure{
 
     public void createMenuPanel(){
 
-        JLabel tempJLabel = new JLabel(null, null, HEIGHT)
+        JLabel tempJLabel = new JLabel()
 
         Font scoreFont = new Font("Arial",Font.BOLD,35);
         JLabel scoreLabel = new JLabel("You matched "+GameLayout.matches+" Cards in "+GameLayout.turns+" Turns !");
