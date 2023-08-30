0066976e005c1595d4d055b94d88c83b6a3cef3c
Devang Gentyal <darshangentyal02@gmail.com>
2023-08-30 15:41:54 +0530

    Made Few changes

--- external/Cards.java
+++ external/Cards.java
@@ -125,6 +125,14 @@ public class Cards implements ActionListener {
 
                         GameLayout.turns -= 1;
                         GameLayout.turnsLabel.setText("Turns left : " + GameLayout.turns);
+                        if (GameLayout.matches == 1 || GameLayout.turns == 0) {
+                            // Call Ending Screen Method
+                            EndingLayout endingLayout = new EndingLayout(app);
+                            app.remove(thisLayout.getGamePanel());
+                            app.add(endingLayout.getEndingPanel(), BorderLayout.CENTER);
+                            app.revalidate();
+                            app.repaint();
+                        }
                     }
                 });
                 timer.setRepeats(false);
