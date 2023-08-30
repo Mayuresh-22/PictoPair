9f879a5135a1433152a9f41815f08b148777668e
Devang Gentyal <darshangentyal02@gmail.com>
2023-08-30 14:01:38 +0530

    Made Few changes

--- layout/EndingLayout.java
+++ layout/EndingLayout.java
@@ -63,7 +63,7 @@ public class EndingLayout implements ScreenStructure{
         JLabel tempJLabel = new JLabel("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
         tempJLabel.setFont(new Font("Arial",Font.BOLD,10));
         tempJLabel.setOpaque(false);
-        tempJLabel.setOpaque(false);
+        tempJLabel.setVisible(false);;
 
 
 
