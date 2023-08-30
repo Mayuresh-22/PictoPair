3f06513458713926f39c4a91c5fa53d131938d46
Devang Gentyal <darshangentyal02@gmail.com>
2023-08-30 15:52:17 +0530

    Made Few changes

--- layout/EndingLayout.java
+++ layout/EndingLayout.java
@@ -33,7 +33,7 @@ public class EndingLayout implements ScreenStructure{
     }
 
     public void getthisLayout(EndingLayout thisLayout){
-        thisLayout = thisLayout;
+        thisLayout = tempthisLayout;
     }
     
     public JPanel getEndingPanel() {
