be7b132ec9e65c228a996528ffb7efc8cbad05ab
Devang Gentyal <darshangentyal02@gmail.com>
2023-08-30 14:02:46 +0530

    Made Few changes

--- layout/EndingLayout.java
+++ layout/EndingLayout.java
@@ -62,7 +62,7 @@ public class EndingLayout implements ScreenStructure{
 
         JPanel tempPanel = new JPanel();
         tempPanel.setBackground(null);
-        tempPanel.set
+        tempPanel.setSize(200, HEIGHT);
 
 
 
