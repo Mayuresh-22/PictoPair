f2dff543e92485ecbf62071dad6c2715acef96e0
Devang Gentyal <darshangentyal02@gmail.com>
2023-08-30 13:48:33 +0530

    Made Few changes

--- layout/EndingLayout.java
+++ layout/EndingLayout.java
@@ -61,6 +61,7 @@ public class EndingLayout implements ScreenStructure{
     public void createMenuPanel(){
 
         JLabel scoreLabel = new JLabel("You Matched "+GameLayout.matches+" in "+GameLayout.turns+" turns !");
+        
 
         play = createButton(play, "PLAY", 0, 0, 200, 50, 20);
         play.addActionListener(new ActionListener(){
