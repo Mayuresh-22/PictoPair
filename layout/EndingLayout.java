3e06ee640cc3294b628f58cbdead88f2ffa43a1c
Devang Gentyal <darshangentyal02@gmail.com>
2023-08-30 14:00:02 +0530

    Made Few changes

--- layout/EndingLayout.java
+++ layout/EndingLayout.java
@@ -61,7 +61,7 @@ public class EndingLayout implements ScreenStructure{
     public void createMenuPanel(){
 
         JLabel tempJLabel = new JLabel("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-        tempJLabel.setFont(new Font("Arial",Font.BOLD,50);
+        tempJLabel.setFont(new Font("Arial",Font.BOLD,50));
 
 
         Font scoreFont = new Font("Arial",Font.BOLD,35);
