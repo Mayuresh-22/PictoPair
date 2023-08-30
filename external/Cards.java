1e76446d9da2d5d2d258fe859c9ff04b8f90af9e
Devang Gentyal <darshangentyal02@gmail.com>
2023-08-30 14:17:04 +0530

    Made Few changes

--- external/Cards.java
+++ external/Cards.java
@@ -18,7 +18,7 @@ public class Cards implements ActionListener {
     public JButton button;
     public String status, path;
     public ImageIcon defaultImg, mainImg;
-    JFrame static app;
+    JFrame  app;
     GameLayout thisLayout;
 
     public Cards(String status, ImageIcon mainImg, ImageIcon defaultImg, String path ,int id) {
