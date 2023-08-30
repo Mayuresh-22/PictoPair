8aecd0a26ac40e5a00b5115a9094e6fa63a72413
Devang Gentyal <darshangentyal02@gmail.com>
2023-08-30 16:02:35 +0530

    Made Few changes

--- layout/EndingLayout.java
+++ layout/EndingLayout.java
@@ -158,8 +158,6 @@ public class EndingLayout implements ScreenStructure{
         yes.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 message.setText("Thank you for playing! Exiting...");
-                yes.setVisible(false);
-                no.setVisible(false);
                 Timer t = new Timer(1500, new ActionListener(){
                     public void actionPerformed(ActionEvent e){
                         System.exit(0);
