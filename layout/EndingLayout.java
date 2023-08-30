5abc4e951c1bac4d4044fd01ba26153de565c1fb
Devang Gentyal <darshangentyal02@gmail.com>
2023-08-30 14:00:40 +0530

    Made Few changes

--- layout/EndingLayout.java
+++ layout/EndingLayout.java
@@ -102,7 +102,7 @@ public class EndingLayout implements ScreenStructure{
         buttonsPanel.add(quite);
 
         // Adding to menuPanel with flow layout
-        menuPanel.add(tempJLabel,BorderLayout.CENTER);
+        menuPanel.add(tempJLabel,BorderLayout.);
         menuPanel.add(scoreLabel,BorderLayout.CENTER);
         menuPanel.add(buttonsPanel,BorderLayout.SOUTH);
 
