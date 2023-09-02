f31ba5f385414dd640b3e61ba53e842f9d43607f
Mayuresh <mayureshchoudhary22@gmail.com>
2023-09-02 13:01:13 +0530

    Made Few changes

--- PictoPair.java
+++ PictoPair.java
@@ -27,6 +27,7 @@ class App {
                 app.repaint();
                 musicPlayer.filePath = "assets/sounds/bgm.wav";
                 musicPlayer.loop = true;
+                musicPlayer.start();
                 Timer t = new Timer(1000, new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
