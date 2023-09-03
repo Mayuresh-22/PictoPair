aff867f44a4d6d69a3a13d99de7c00236a598b2e
Mayuresh <mayureshchoudhary22@gmail.com>
2023-09-03 15:10:54 +0530

    Made Few changes

--- external/Cards.java
+++ external/Cards.java
@@ -85,9 +85,17 @@ public class Cards implements ActionListener {
 
                         GameLayout.matches += 1;
                         GameLayout.turns -= 1;
-
                         GameLayout.matchesLabel.setText("Matches : " + GameLayout.matches);
                         GameLayout.turnsLabel.setText("Turns left : " + GameLayout.turns);
+
+                        if (GameLayout.matches == 12 || GameLayout.turns == 0) {
+                            // Call Ending Screen Method
+                            EndingLayout endingLayout = new EndingLayout(app);
+                            app.remove(thisLayout.getGamePanel());
+                            app.add(endingLayout.getEndingPanel(), BorderLayout.CENTER);
+                            app.revalidate();
+                            app.repaint();
+                        }
 
                     }
                 });
