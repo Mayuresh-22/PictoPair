dfd2f2c1205a53ba79048d1b41ea5c36cba2fc88
Mayuresh <mayureshchoudhary22@gmail.com>
2023-09-03 15:17:50 +0530

    Made Few changes

--- external/Cards.java
+++ external/Cards.java
@@ -128,6 +128,7 @@ public class Cards implements ActionListener {
                         GameLayout.turns -= 1;
                         GameLayout.turnsLabel.setText("Turns left : " + GameLayout.turns);
 
+                        // Checking if game is over
                         if (GameLayout.matches == 12 || GameLayout.turns == 0) {
                             // Call Ending Screen Method
                             EndingLayout endingLayout = new EndingLayout(app);
