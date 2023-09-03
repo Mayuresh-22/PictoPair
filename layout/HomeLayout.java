fd111c77bf5712ad4b8c815625df410e26ccc57b
Mayuresh <mayureshchoudhary22@gmail.com>
2023-09-03 14:54:20 +0530

    Made Few changes

--- layout/HomeLayout.java
+++ layout/HomeLayout.java
@@ -63,7 +63,7 @@ public class HomeLayout implements ScreenStructure{
     }
 
     public void createMenuPanel(){
-        play = createButton(play, "PLAY", 0, 0, 500, 100, 40);
+        play = createButton(play, "PLAY", new Color(77,0,206,255), 0, 0, 500, 100, 40);
         play.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 // Play GameLayout
