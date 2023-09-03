bfbcd08567729fd37303daa3af7ac31226e834ab
Mayuresh <mayureshchoudhary22@gmail.com>
2023-09-03 15:20:45 +0530

    Made Few changes

--- layout/EndingLayout.java
+++ layout/EndingLayout.java
@@ -67,7 +67,7 @@ public class EndingLayout implements ScreenStructure{
 
 
         Font scoreFont = new Font("Arial",Font.BOLD,25);
-        JLabel scoreLabel = new JLabel("You matched "+GameLayout.matches+" Cards in "+GameLayout.turns+" Turns !");
+        JLabel scoreLabel = new JLabel("You matched "+GameLayout.matches+" Cards in "+(30 - GameLayout.turns)+" Turns !");
         scoreLabel.setFont(scoreFont);
         scoreLabel.setForeground(Color.WHITE);
         scoreLabel.setVerticalAlignment(JLabel.CENTER);
