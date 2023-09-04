8ec2c07071ec46f6889785c6feeec9b235457b2b
Devang Gentyal <darshangentyal02@gmail.com>
2023-09-04 12:23:24 +0530

    Made Few changes

--- external/Cards.java
+++ external/Cards.java
@@ -125,7 +125,7 @@ public class Cards implements ActionListener {
                         c4.status = "hidden";
 
                         GameLayout.turns -= 1;
-                        GameLayout.matches = 1;
+                        GameLayout.matches += 1;
                         GameLayout.turnsLabel.setText("Turns left : " + GameLayout.turns);
                         if (GameLayout.matches == 12 || GameLayout.turns <= 0) {
                             // Call Ending Screen Method
