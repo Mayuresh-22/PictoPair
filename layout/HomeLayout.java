a7a7a62abf041da0b1af467a60647aa8bad30b5d
Devang Gentyal <darshangentyal02@gmail.com>
2023-09-04 14:17:20 +0530

    Made Few changes

--- layout/HomeLayout.java
+++ layout/HomeLayout.java
@@ -49,6 +49,7 @@ public class HomeLayout implements ScreenStructure {
         HomePanel.add(layeredPane);
         HomePanel.setOpaque(true);
     }
+    
     GameLayout gameLayout = new GameLayout(app);
 
     public void getthisLayout(HomeLayout thisLayout) {
