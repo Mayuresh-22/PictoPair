a2f54e093d0aaf2ff68dd8ec35a05a603012b67d
Devang Gentyal <darshangentyal02@gmail.com>
2023-09-04 13:31:11 +0530

    Made Few changes

--- layout/HomeLayout.java
+++ layout/HomeLayout.java
@@ -87,7 +87,7 @@ public class HomeLayout implements ScreenStructure {
                 layeredPane.repaint();
 
                 //Remove LoadingPanel after 2.5 seconds 
-                Timer timer = new Timer(2500, new ActionListener() {
+                Timer timer = new Timer(5500, new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
                         // Play GameLayout
