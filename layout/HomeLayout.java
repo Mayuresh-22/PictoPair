17abf00e501fe4f77a374a34ce73ac2086d70ccf
Devang Gentyal <darshangentyal02@gmail.com>
2023-09-04 13:27:56 +0530

    Made Few changes

--- layout/HomeLayout.java
+++ layout/HomeLayout.java
@@ -77,7 +77,7 @@ public class HomeLayout implements ScreenStructure{
         play.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 // Loading animation
-                app.remove();
+                layeredPane.remove(menuPanel);
                 // Play GameLayout
                 GameLayout gameLayout = new GameLayout(app);
                 app.remove(thisLayout.getHomePanel());
