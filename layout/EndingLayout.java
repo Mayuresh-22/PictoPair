fa571793e8326b5857f30018d4e862f0a193ac52
Devang Gentyal <darshangentyal02@gmail.com>
2023-09-04 12:15:35 +0530

    Made Few changes

--- layout/EndingLayout.java
+++ layout/EndingLayout.java
@@ -84,7 +84,7 @@ public class EndingLayout implements ScreenStructure{
                 GameLayout.turns = 0;
                 // Play GameLayout
                 GameLayout gameLayout = new GameLayout(app);
-                app.remove(this.getEndingPanel);
+                app.remove(this.getEndingPanel());
                 app.add(gameLayout.getGamePanel(), BorderLayout.CENTER);
                 app.revalidate();
                 app.repaint();
