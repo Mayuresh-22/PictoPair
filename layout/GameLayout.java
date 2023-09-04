6ac5a0edde4faabb91fc26edf44217975ef8b37d
Devang Gentyal <darshangentyal02@gmail.com>
2023-09-04 13:04:19 +0530

    Made Few changes

--- layout/GameLayout.java
+++ layout/GameLayout.java
@@ -16,7 +16,7 @@ public class GameLayout implements ScreenStructure, ActionListener {
             scorePanel = new JPanel();
     JLabel bg;
     JLayeredPane layeredPane = new JLayeredPane();
-    JButton quitButton = new JButton("H");
+    JButton quitButton = new JButton("QUIT");
     JFrame app;
 
     public static int matches = 0,turns = 30;
