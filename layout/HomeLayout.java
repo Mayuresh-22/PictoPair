79d2cdeaf3720f0f3d66988d08565ff10820ed98
Devang Gentyal <darshangentyal02@gmail.com>
2023-09-04 13:06:24 +0530

    Made Few changes

--- layout/HomeLayout.java
+++ layout/HomeLayout.java
@@ -10,6 +10,7 @@ import interfaces.*;
 
 public class HomeLayout implements ScreenStructure{
     JPanel HomePanel = new JPanel(null), bgPanel = new JPanel(new BorderLayout()), menuPanel = new JPanel(new FlowLayout(FlowLayout.CENTER,50, 40)), loadingPanel = new JPanel(new GridBagLayout());
+    
     JLabel bg;
     JLayeredPane layeredPane = new JLayeredPane();
     JButton play, settings, quite, yes, no, musicOptionButton, gridOptionButton;
