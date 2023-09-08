42e27442287f6bb24c6870e7d69bb52d145cb2e1
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-09-08 12:46:51 +0530

    Made Few changes

--- constants/Constants.java
+++ constants/Constants.java
@@ -11,5 +11,7 @@ public class Constants {
     public static final String ENDING_SCREEN_IMAGE_PATH = "assets/images/EndingScreen-bg.png";
     public static final String HOME_SCREEN_IMAGE_PATH = "assets/images/HomeScreen-bg.png";
     public static final String LOADING_SCREEN_IMAGE_PATH = "assets/images/LoadingScreen-bg.png";
+    public static int WIDTH = (int) screenSize.getWidth();
+    public static int HEIGHT = (int) screenSize.getHeight();
     public static ImageIcon defaultMusicIcon = new ImageIcon(new ImageIcon("assets/images/sound-on.png").getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH));
 }
