7903d6f969e47d45ca5ce1d8c02340cfbf3eddd1
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-09-08 12:30:53 +0530

    Made Few changes

--- layout/HomeLayout.java
+++ layout/HomeLayout.java
@@ -95,7 +95,7 @@ public class HomeLayout implements ScreenStructure {
                     public void actionPerformed(ActionEvent e) {
                         // Play GameLayout
                         GameLayout gameLayout = new GameLayout(app);
-                        app.remove(thisgetHomePanel());
+                        app.remove(this.getHomePanel());
                         app.add(gameLayout.getGamePanel(), BorderLayout.CENTER);
                         app.revalidate();
                         app.repaint();
