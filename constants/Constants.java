e967221a3d7c48237758deee18dc347569c00107
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-09-08 12:38:23 +0530

    Made Few changes

--- constants/Constants.java
+++ constants/Constants.java
@@ -1,11 +1,15 @@
 package constants;
 
+import javax.swing.ImageIcon;
+
 public class Constants {
-    // create a constant for music
+    // create a constant
     public static final String MUSIC_FILE_PATH = "assets/sounds/bgm.wav";
     public static final String SOUND_EFFECT_FILE_PATH = "assets/sounds/intro.wav";
     public static final String CARD_BACK_IMAGE_PATH = "assets/images/card-back.png";
     public static final String ENDING_SCREEN_IMAGE_PATH = "assets/images/EndingScreen-bg.png";
     public static final String HOME_SCREEN_IMAGE_PATH = "assets/images/HomeScreen-bg.png";
     public static final String LOADING_SCREEN_IMAGE_PATH = "assets/images/LoadingScreen-bg.png";
+    ImageIcon def;
+    public static final ImageIcon defaultMusicIcon = new ImageIcon("assets/images/music.png");
 }
