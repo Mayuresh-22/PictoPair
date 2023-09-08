f32c021e57245906281a960ae2a2e35d74386cba
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-09-08 12:33:55 +0530

    Made Few changes

--- layout/EndingLayout.java
+++ layout/EndingLayout.java
@@ -129,7 +129,7 @@ public class EndingLayout implements ScreenStructure {
                 layeredPane.repaint();
 
                 // Remove LoadingPanel after 5 seconds
-                Timer timer = new Timer(5000, new ActionListener() {
+                Timer timer = new Timer(2000, new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
                         // Play GameLayout
