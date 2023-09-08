5438d03535463cfe08118b5cbb6ecf76d4ea38d0
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-09-08 12:21:21 +0530

    Made Few changes

--- layout/EndingLayout.java
+++ layout/EndingLayout.java
@@ -133,7 +133,7 @@ public class EndingLayout implements ScreenStructure {
                     @Override
                     public void actionPerformed(ActionEvent e) {
                         // Play GameLayout
-                        HomeLayout homeLayout = new HomeLayout(app);
+                        HomeLayout homeLayout = new HomeLayout(app, null);
                         app.remove(EndingLayout.this.getEndingPanel());
                         app.add(homeLayout.getHomePanel(), BorderLayout.CENTER);
                         app.revalidate();
