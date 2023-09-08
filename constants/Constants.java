821ccad13899a686ef74912e11af707b2817de44
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-09-08 12:55:09 +0530

    Made Few changes

--- constants/Constants.java
+++ constants/Constants.java
@@ -18,5 +18,5 @@ public class Constants {
 
     // create a constant for color
     public static final Color COLOR_PRIMARY = new Color(131, 0, 255, 255);
-    public static final Color COLOR_BUTTON = new Color(77, 0, 206, 255);
+    public static final Color  = new Color(77, 0, 206, 255);
 }
