b8c708c49501da5619ae85836aaace9c3af987c7
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2023-09-08 12:37:17 +0530

    Made Few changes

---
+++ constants/Constants.java
@@ -0,0 +1,11 @@
+package constants;
+
+public class Constants {
+    // create a constant for music
+    public static final String MUSIC_FILE_PATH = "assets/sounds/bgm.wav";
+    public static final String SOUND_EFFECT_FILE_PATH = "assets/sounds/intro.wav";
+    public static final String CARD_BACK_IMAGE_PATH = "assets/images/card-back.png";
+    public static final String ENDING_SCREEN_IMAGE_PATH = "assets/images/EndingScreen-bg.png";
+    public static final String HOME_SCREEN_IMAGE_PATH = "assets/images/HomeScreen-bg.png";
+    public static final String LOADING_SCREEN_IMAGE_PATH = "assets/images/LoadingScreen-bg.png";
+}
