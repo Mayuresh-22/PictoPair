3b2723fb137bcbc32633d347c515e121d6a97aaa
Devang Gentyal <darshangentyal02@gmail.com>
2023-10-29 14:46:01 +0530

    Made Few changes

--- layout/HomeLayout.java
+++ layout/HomeLayout.java
@@ -203,7 +203,7 @@ public class HomeLayout implements ScreenStructure {
         gridOptionComboBox = new JComboBox<>(items);
         gridOptionComboBox.setBackground(new Color(77, 0, 206, 255));
         gridOptionComboBox.setForeground(Color.white);
-        gridOptionComboBox.set
+        gridOptionComboBox.setR
 
         musicOptionButton = createButton(musicOptionButton, "", null, 0, 0, 200, 100, 20);
         musicOptionButton.setIcon(def);
