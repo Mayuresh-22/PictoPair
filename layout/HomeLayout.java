405cad957dc073c1a66577745b4e913c1a76b854
Devang Gentyal <darshangentyal02@gmail.com>
2023-10-29 14:53:51 +0530

    Made Few changes

--- layout/HomeLayout.java
+++ layout/HomeLayout.java
@@ -207,7 +207,7 @@ public class HomeLayout implements ScreenStructure {
         gridOptionComboBox.setForeground(Color.white);
         gridOptionComboBox.setFont(new Font("Arial", Font.BOLD, 20));
         // Assigning rows and columns based on selection
-        gridOptionComboBox.addActionListener(new);
+        gridOptionComboBox.addActionListener(new ActionListner);
 
         musicOptionButton = createButton(musicOptionButton, "", null, 0, 0, 200, 100, 20);
         musicOptionButton.setIcon(def);
