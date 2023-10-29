c45dcd367401d0b4e67072c5600209f85bec1395
Devang Gentyal <darshangentyal02@gmail.com>
2023-10-29 15:17:10 +0530

    Made Few changes

--- layout/EndingLayout.java
+++ layout/EndingLayout.java
@@ -16,7 +16,7 @@ public class EndingLayout implements ScreenStructure {
     JFrame app;
     EndingLayout thisLayout;
     int matches, turns;
-
+    GameLayout gameLayout = new GameLayout(app);
     
 
     public EndingLayout(JFrame app) {
