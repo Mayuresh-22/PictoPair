b2e727fc7977f6983a6952fa8bf028dc74b265db
Devang Gentyal <darshangentyal02@gmail.com>
2023-10-29 15:26:41 +0530

    Made Few changes

--- PictoPair.java
+++ PictoPair.java
@@ -46,7 +46,7 @@ class App {
         app.setLayout(new BorderLayout(100, 100));
         app.setIconImage(new ImageIcon("assets/images/card-back.png").getImage());
 
-        // M
+        // Restore Down size if pressed Esc
         app.addKeyListener(new KeyAdapter() {
             private boolean fullScreen = true;
 
