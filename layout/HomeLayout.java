d6f7176551e18bbc492c45b6ac3b1c3b9175f48e
Devang Gentyal <darshangentyal02@gmail.com>
2023-10-29 14:39:04 +0530

    Made Few changes

--- layout/HomeLayout.java
+++ layout/HomeLayout.java
@@ -15,7 +15,7 @@ public class HomeLayout implements ScreenStructure {
     JLabel bg;
     JLayeredPane layeredPane = new JLayeredPane();
     JButton play, settings, quite, yes, no, musicOptionButton, gridOptionButton;
-    
+    JComboBox<String> comboBox
     JFrame app;
     ImageIcon musicOn, musicOff, def;
     HomeLayout thisLayout;
