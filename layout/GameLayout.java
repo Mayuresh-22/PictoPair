6f7c011feee6723a44a987ee2edb539a7999c543
Devang Gentyal <darshangentyal02@gmail.com>
2023-10-29 14:57:11 +0530

    Made Few changes

--- layout/GameLayout.java
+++ layout/GameLayout.java
@@ -30,7 +30,7 @@ public class GameLayout implements ScreenStructure, ActionListener {
         this.app = app;
 
         // Setting gridRows and gridColumns
-        
+        gri
 
         // reseting matches and turns
         matches = 0;
