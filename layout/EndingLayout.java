b51dfd6569d682f5ac472d81486975be9a7bf34f
Devang Gentyal <darshangentyal02@gmail.com>
2023-10-29 15:22:54 +0530

    Made Few changes

--- layout/EndingLayout.java
+++ layout/EndingLayout.java
@@ -104,6 +104,7 @@ public class EndingLayout implements ScreenStructure {
                     @Override
                     public void actionPerformed(ActionEvent e) {
                         // Play GameLayout
+                         GameLayout gameLayout = new GameLayout();
                         GameLayout.app = app;
                         Cards.app = app;
                         GameLayout.matches = 0;
